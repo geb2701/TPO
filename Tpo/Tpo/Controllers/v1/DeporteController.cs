9bd16e92d7e6afcc55c310bf01719eb81dd8c4e0
Alejandro Gabriel Guerrero <alejandro.g.guerrero@hotmail.com>
2025-06-22 19:47:43 -0300

    DeporteControllerList

--- Tpo/Tpo/Controllers/v1/DeporteController.cs
+++ Tpo/Tpo/Controllers/v1/DeporteController.cs
@@ -1,18 +1,17 @@
 ﻿using Asp.Versioning;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+using System.Text.Json;
 using Tpo.Attributes;
 using Tpo.Domain.Deporte.Dtos;
 using Tpo.Domain.Deporte.Features;
-using Tpo.Domain.Usuario.Features;
 
 namespace Tpo.Controllers.v1
 {
     [ApiController]
     [Route("api/v{v:apiVersion}/[controller]")]
     [ApiVersion("1.0")]
-    [Authorize]
+   
     public class DeporteController(ILogger<DeporteController> logger, IMediator mediator) : ControllerBase
     {
         private readonly ILogger<DeporteController> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
@@ -25,6 +24,33 @@ namespace Tpo.Controllers.v1
             var queryResponse = await _mediator.Send(command);
             return Ok(queryResponse);
         }
+
+
+        [HttpGet(Name = "GetDeportes")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetList([FromQuery] DeporteParametersDto parametersDto)
+        {
+            var query = new GetDeportesList.Query(parametersDto);
+            var queryResponse = await _mediator.Send(query);
+
+            var paginationMetadata = new
+            {
+                totalCount = queryResponse.TotalCount,
+                pageSize = queryResponse.PageSize,
+                currentPageSize = queryResponse.CurrentPageSize,
+                currentStartIndex = queryResponse.CurrentStartIndex,
+                currentEndIndex = queryResponse.CurrentEndIndex,
+                pageNumber = queryResponse.PageNumber,
+                totalPages = queryResponse.TotalPages,
+                hasPrevious = queryResponse.HasPrevious,
+                hasNext = queryResponse.HasNext
+            };
+
+            Response.Headers["X-Pagination"] = JsonSerializer.Serialize(paginationMetadata);
+
+            return Ok(queryResponse);
+        }
+
     }
 }
 
