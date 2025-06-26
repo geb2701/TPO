6f9793ced604ce1a34f1f490b8d6096a1d44aa0b
Nicolás Ventieri <38119202+Nicoseal@users.noreply.github.com>
2025-06-26 00:09:01 -0300

    Confirmar jugadores (#1)
    * features for confirmations added
    
    * endpoints for confirmations added

--- Tpo/Tpo/Controllers/v1/ConfirmacionPartido.cs
+++ Tpo/Tpo/Controllers/v1/ConfirmacionPartido.cs
@@ -2,6 +2,8 @@
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Tpo.Domain.Jugador.Features;
+using Tpo.Domain.Partido.Features;
 
 namespace Tpo.Controllers.v1
 {
@@ -12,5 +14,26 @@ namespace Tpo.Controllers.v1
     public class ConfirmacionPartido(IMediator mediator) : ControllerBase
     {
         private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        
+        [HttpPost("confirmar-propio/{partidoId}")]
+        public async Task<IActionResult> ConfirmarPropio([FromRoute] int partidoId)
+        {
+            await _mediator.Send(new ConfirmarPropio.Command(partidoId));
+            return Ok("Jugador confirmado");
+        }
+
+        [HttpPost("confirmar-otro/{jugadorId}")]
+        public async Task<IActionResult> ConfirmarOtro([FromRoute] int jugadorId)
+        {
+            await _mediator.Send(new ConfirmarJugador.Command(jugadorId));
+            return Ok("Jugador confirmado");
+        }
+
+        [HttpPost("confirmar-todos/{partidoId}")]
+        public async Task<IActionResult> ConfirmarTodos([FromRoute] int partidoId)
+        {
+            await _mediator.Send(new ConfirmarTodosJugadores.Command(partidoId));
+            return Ok("Todos los jugadores confirmados");
+        }
     }
 }
