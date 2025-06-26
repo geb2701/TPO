fc03b36d766b8d07bf27e9fa14df4ead0e95609f
Gustavo Bruno <86382687+geb2701@users.noreply.github.com>
2025-06-25 23:41:57 -0300

    Add ConfirmacionPartido v1 API controller
    Introduces the ConfirmacionPartido controller for API version 1.0 with authorization and MediatR dependency injection. This sets up the basic structure for future endpoint implementations.

---
+++ Tpo/Tpo/Controllers/v1/ConfirmacionPartido.cs
@@ -0,0 +1,16 @@
+﻿using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tpo.Controllers.v1
+{
+    [ApiController]
+    [Route("api/v{v:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [Authorize]
+    public class ConfirmacionPartido(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+}
